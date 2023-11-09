791eaaec6465d0e24ef342fcbab16260ffd6144c
Akash Raj <akashraj9828@gmail.com>
2023-11-09 15:45:10 +0530

    Update loader to have skills seperately in db, update gitignore

--- couldinary.js
+++ couldinary.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-const path = require('path')
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const path = require("path");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  api_secret: process.env.API_SECRET,
 });
 
-const imagePath = path.join(__dirname + "Pokemon-Data/images/img")
+const imagePath = path.join(__dirname + "Pokemon-Data/images/img");
 
-  imagePath.map(item=> {
-    
-  })
+imagePath.map((item) => {});
--- models/pokemons.js
+++ models/pokemons.js
@@ -1,34 +1,32 @@
 // Module dependencies.
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const pokemon = new Schema({
-    base: {
-        Attack: Number,
-        Defense: Number,
-        HP: Number,
-        SpAtk: Number,
-        SpDef: Number,
-        Speed: Number,
-    },
-    cname: String,
-    ename: String,
-    id: Number,
-    jname: String,
-    image:String,
-    skills: {
-        egg: [Number],
-        level_up: [Number],
-        tm: [Number],
-        transfer: [Number],
-        tutor: [Number],
-    },
-    type: [{ type: Schema.Types.ObjectId, ref: 'Type' }]
-
+  base: {
+    Attack: Number,
+    Defense: Number,
+    HP: Number,
+    SpAtk: Number,
+    SpDef: Number,
+    Speed: Number,
+  },
+  cname: String,
+  ename: String,
+  id: Number,
+  jname: String,
+  image: String,
+  skills: {
+    egg: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    level_up: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    tm: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    transfer: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    tutor: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+  },
+  type: [{ type: Schema.Types.ObjectId, ref: "Type" }],
 });
 
-
-let Pokemon = mongoose.model('Pokemon', pokemon);
+let Pokemon = mongoose.model("Pokemon", pokemon);
 
 module.exports = Pokemon;
